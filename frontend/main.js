280add16b6e3beb49560a9136254b486cc6b6998
Roland Kutas <roland.kutas@dmgmori-heitec.hu>
2023-09-18 17:33:13 +0000

    Setup task list and basic functionality

--- frontend/main.js
+++ frontend/main.js
@@ -1,29 +1,47 @@
 // const { default: axios } = require("axios");
 
-async function fetchTasks() {
-    const response = await axios.get('http://celosnext.com:8081/todo/tasks');
-    return response.data;
+let tasks;
+
+setup();
+
+function setup() {
+    const addBtn = document.querySelector('.new-todo button');
+    addBtn.addEventListener('click', createTask);
+    refreshTaskList();
 }
 
-function showTasks(tasks) {
+function refreshTaskList() {
+    fetchTasks()
+        .then(() => showTasks())
+        .catch(error => console.error(error));
+}
+
+async function fetchTasks() {
+    const response = await axios.get('http://celosnext.com:8081/todo/tasks');
+    tasks = response.data;
+}
+
+function showTasks() {
+    const main = document.querySelector('.content main');
+    main.replaceChildren()
     tasks.forEach(task => {
-        const main = document.querySelector('.content main');
         let dueAt = task.DueAt;
         if (dueAt) {
             dueAt = new Date(task.DueAt).toLocaleDateString('hu-HU');
         }
-        const taskE = createNewTaskElement(task.Title, dueAt);
+        const taskE = createNewTaskElement(task.ID, task.Title, dueAt);
         main.appendChild(taskE);
     });
 }
 
-function createNewTaskElement(title, dueAt) {
+function createNewTaskElement(id, title, dueAt) {
     const taskE = createElement('div', 'task');
     taskE.appendChild(createElement('span', 'title', title));
     taskE.appendChild(createElement('span', 'due-at', dueAt));
 
     delBtn = createElement('button');
     delBtn.appendChild(createElement('img', 'del-btn'));
+    delBtn.taskId = id;
     delBtn.addEventListener('click', deleteTask);
     taskE.appendChild(delBtn);
 
@@ -41,8 +59,22 @@ function createElement(tag, clazz, textContent) {
     return element;
 }
 
-function deleteTask() {
-    alert('del');
+async function createTask() {
+    let newTask = {
+        "title": "random task"
+    };
+    await axios.post('http://celosnext.com:8081/todo/tasks', newTask)
+        .then(() => refreshTaskList())
+        .catch(error => console.error(error));
 }
 
-fetchTasks().then((r) => showTasks(r));
+async function deleteTask(event) {
+    const taskId = event.currentTarget.taskId;
+    if (confirm('Are you sure to delete this item?')) {
+        tasks = tasks.filter((t) => t.ID !== taskId);
+        showTasks();
+        axios.delete(`http://celosnext.com:8081/todo/tasks/${taskId}`)
+            .then(() => refreshTaskList())
+            .catch(error => console.error(error));
+    }
+}
